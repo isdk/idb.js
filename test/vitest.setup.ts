3f1321c5c118eb9138fb05e129777414567c9e3d
Riceball LEE <snowyu.lee@gmail.com>
2023-08-16 19:37:38 +0800

    test: pr to fake-indexeddb to resolve no close event problem

--- test/vitest.setup.ts
+++ test/vitest.setup.ts
@@ -1,1 +1,22 @@
 import 'fake-indexeddb/auto'
+
+/*
+import { IDBDatabase, IDBVersionChangeEvent } from 'fake-indexeddb'
+
+const FakeEvent = Object.getPrototypeOf(
+  IDBVersionChangeEvent.prototype
+).constructor
+
+const close = IDBDatabase.prototype.close
+
+IDBDatabase.prototype.close = function () {
+  close.call(this)
+  const interval = setInterval(() => {
+    if (this._closed) {
+      clearInterval(interval)
+      const evt = new FakeEvent('close') // { type: 'close', initialized: true, eventPath: }
+      this.dispatchEvent(evt)
+    }
+  }, 50)
+}
+*/
