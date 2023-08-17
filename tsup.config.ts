4346aaec8fab07726e01772d6760794c15cae3bc
Riceball LEE <snowyu.lee@gmail.com>
2023-08-17 17:05:56 +0800

    build: update build script

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,21 @@
+import process from 'node:process'
+
 import { defineConfig } from 'tsup'
 
+const env = process.env.NODE_ENV
+
 export default defineConfig({
-  entry: ['src/index.ts', 'src/bn.ts', 'src/native.ts'],
+  splitting: true,
+  // include all files under src
+  entry: ['src/**/*.ts'],
   format: ['cjs', 'esm'],
-  // splitting: true,
-  // sourcemap: true,
-  // clean: true,
+  dts: true, // generate dts file for main module
+  bundle: env === 'production',
+  minify: env === 'production',
+  watch: env === 'development',
+  outDir: env === 'production' ? 'dist' : 'lib',
+  skipNodeModulesBundle: true,
+  target: 'es2020',
+  sourcemap: env === 'prod', // source map is only available in prod
+  clean: true, // rimraf disr
 })
